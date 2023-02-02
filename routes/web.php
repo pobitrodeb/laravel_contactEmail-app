aebc101bbdf921f32093318aa29c8ce83242e682
Pobitrodeb <pobitrodn@gmail.com>
2023-02-02 23:29:43 +0600

    Contact Controller crate

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact-us', [ContactController::class, 'index']);
+Route::post('contact-us', [ContactController::class, 'store'])->name('contact.us.store');
