f32b0e716e90d0186282cb1ef6df3694fab95adc
Pobitrodeb <pobitrodn@gmail.com>
2023-02-02 23:26:36 +0600

    contact model create

---
+++ app/Models/Contact.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Mail;
+
+class Contact extends Model
+{
+    use HasFactory;
+    public $fillable = ['name', 'email', 'phone', 'subject', 'message'];
+
+    public static function boot() {
+
+        parent::boot();
+
+        static::created(function ($item) {
+
+            $adminEmail = "your_admin_email@gmail.com";
+            Mail::to($adminEmail)->send(new ContactMail($item));
+        });
+    }
+}
